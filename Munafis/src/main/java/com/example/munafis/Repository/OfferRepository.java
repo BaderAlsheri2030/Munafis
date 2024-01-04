31fe6e44f78df13729fabc9adf6cc5aa10394989
nadaanasser <148685934+NadaaNasserr@users.noreply.github.com>
2024-01-04 14:33:37 +0300

    OfferRepository
    OfferRepository

--- Munafis/src/main/java/com/example/munafis/Repository/OfferRepository.java
+++ Munafis/src/main/java/com/example/munafis/Repository/OfferRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class OfferRepository {
+public interface OfferRepository extends JpaRepository<Offers,Integer> {
 }
