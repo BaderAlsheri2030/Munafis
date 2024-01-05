47cbe4dc31157570e3ae4dea7fa802629b978374
nadaanasser <148685934+NadaaNasserr@users.noreply.github.com>
2024-01-05 19:52:56 +0300

    last updated
    last updated

--- Munafis/src/main/java/com/example/munafis/Controller/OrderController.java
+++ Munafis/src/main/java/com/example/munafis/Controller/OrderController.java
@@ -1,4 +1,16 @@
 package com.example.munafis.Controller;
 
+
+import com.example.munafis.Service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/order")
+@RequiredArgsConstructor
 public class OrderController {
+
+
+    private final OrderService orderService;
 }
--- Munafis/src/main/java/com/example/munafis/Service/CompanyService.java
+++ Munafis/src/main/java/com/example/munafis/Service/CompanyService.java
@@ -1,14 +1,56 @@
 package com.example.munafis.Service;
 
 
+import com.example.munafis.API.ApiException;
+import com.example.munafis.Model.Company;
+import com.example.munafis.Model.Provider;
 import com.example.munafis.Repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CompanyService {
 
     private final CompanyRepository companyRepository;
+
+
+    public List getAllCompanies(){
+        return companyRepository.findAll();
+    }
+
+    //Register
+    public void addCompany(Company company){
+        companyRepository.save(company);
+    }
+
+    public void updateCompany(Integer id, Company company){
+        Company oldCompany=companyRepository.findCompanyById(id);
+        if(oldCompany==null){
+            throw new ApiException("company id not found");
+        }
+        oldCompany.setAddress(company.getAddress());
+        oldCompany.setBusinessNumber(company.getBusinessNumber());
+        oldCompany.setOrders(company.getOrders());
+        oldCompany.setCompanyName(company.getCompanyName());
+        oldCompany.setEmail(company.getEmail());
+        oldCompany.setRfps(company.getRfps());
+
+        companyRepository.save(oldCompany);
+
+    }
+
+
+
+    public void deleteCompany(Integer id){
+        Company company=companyRepository.findCompanyById(id);
+        if(company==null){
+            throw new ApiException("company id not found");
+        }
+        companyRepository.delete(company);
+    }
+
 
 }
